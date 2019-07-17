f0350f03df59e591a5f90348b0dd7a4a3dc1a3f5
Ap <arunabh@curlanalytics.com>
2019-07-17 18:56:26 +0530

    updated the script post creation more usable

--- artifacts/scripts/post-creation.sh
+++ artifacts/scripts/post-creation.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+
+export PATH=${PWD}/../../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}
+
+echo $FABRIC_CFG_PATH
+
+CHANNEL_NAME="$1"
+LANGUAGE="$2"
+DELAY="5"
+MAX_RETRY=10
+
+LANGUAGE=node
+CC_SRC_PATH=/opt/gopath/src/github.com/chain-code
+
 # import utils
 . utils.sh
 
+setGlobals 1 1
+# peer version
+
 # To try out things
+
+# installChaincode 1 1
+
 instantiateChaincode 1 1
